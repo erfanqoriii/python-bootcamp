0623c3eedce66c053c14291db470d2f65aa89a7a
erfan <erfanqoristeam@gmail.com>
2025-07-03 13:36:49 +0330

    added 01-basics.py

--- 01-basics.py
+++ 01-basics.py
@@ -1,1 +1,10 @@
-asdasdasd
+name=("enter your name: ")
+age=int(input("enter your age: "))
+
+if age>=18:
+  print(f"{name},you are an adult.")
+else:
+  print(f"{name},you are not an adult yet.")
+for i in range(1,age+1):
+  print(i)
+
